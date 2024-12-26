dea629fc9ac08cf62a1c715b64b5720f628e0f19
atheo810 <atheos810@gmail.com>
2024-12-27 02:05:33 +0800

    feat: lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -3,7 +3,7 @@ return {
   dependencies = { 'nvim-tree/nvim-web-devicons' },
   opts = {
     icon_enabled = true,
-    theme = 'ayu_dark',
+    theme = 'auto',
     component_separators = { left = '', right = '' },
     section_separators = { left = '', right = '' },
     disabled_filetypes = {
@@ -13,6 +13,10 @@ return {
     ignore_focus = {},
     always_divide_middle = true,
     always_show_tabline = true,
+    sections = {
+        lualine_a = {'mode'},
+        lualine_b = {'branch'},
+    },
     globalstatus = false,
     refresh = {
       status_line = 1000,
